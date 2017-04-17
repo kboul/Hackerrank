f7d196d014215acad5f00fb8ade96d249175df9e
kboul <kos.voulgaridis@gmail.com>
2017-04-17 15:34:23 +0300

    Simplify Stdin and Stdout I

--- Java/Introduction/stdin-and-stdout-1.java
+++ Java/Introduction/stdin-and-stdout-1.java
@@ -3,17 +3,9 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        // Complete this line
-        int b = scan.nextInt();
-        // Complete this line
-        int c = scan.nextInt();
-
-        System.out.println(a);
-        // Complete this line
-        System.out.println(b);
-        // Complete this line
-        System.out.println(c);
+        Scanner scan = new Scanner(System.in);        
+        for (int i = 0; i < 3; i++) {
+            System.out.println(scan.nextInt());
+        }
     }
 }
