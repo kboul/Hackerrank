75721c3aafdf91995c19865ef55c79a42c8e4ef2
kboul <kos.voulgaridis@gmail.com>
2017-04-17 12:22:41 +0300

    Simple Array Sum in js

---
+++ Algorithms/Warmup/simple-array-sum.js
@@ -0,0 +1,12 @@
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+    
+    var sum = 0;
+    for (var i =0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    console.log(sum);
+    return sum;
+}
