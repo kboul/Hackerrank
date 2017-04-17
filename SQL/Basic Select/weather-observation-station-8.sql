5c211802cccd16f4b4b279f05612da7bf0f636e0
kboul <kos.voulgaridis@gmail.com>
2017-04-17 13:11:31 +0300

    Weather Observation Station 8 in MySQL

---
+++ SQL/Basic Select/weather-observation-station-8.sql
@@ -0,0 +1,10 @@
+/*
+    Query the list of CITY names from STATION 
+    which have vowels (i.e., a, e, i, o, and u) 
+    as both their first and last characters. 
+    Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT city
+FROM   station
+WHERE  city RLIKE '^[aeiouAEIOU]'AND city RLIKE'[aeiouAEIOU]$';
